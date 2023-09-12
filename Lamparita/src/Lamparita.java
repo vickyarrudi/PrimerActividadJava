795ae4dcb0fecc2bf6386e689889f47ecd91ef03
datascience <varrudi@estudiantes.unsam.edu.ar>
2023-09-12 16:52:47 -0300

    salados

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,20 @@
 public class Lamparita {
+    private boolean estado = false;
+    public void apagar(){
+        this.estado = false;
+    }
+    public void encender(){
+        this.estado = true;
+    }
+    public boolean estado(){
+        return this.estado;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Vamos a programar una lamparita! ");
+
+        System.out.println("Vamos a programar una lamparita!!");
+        Lamparita lam1 = new Lamparita();
+        lam1.apagar();
+        System.out.println(lam1.estado());
     }
 }
